941768f705bae7177375cbddaa61b0a200f23421
coxdoank <harryhandoko@rocketmail.com>
2021-01-26 17:32:56 +0700

    Add files via upload

--- sqlserver/import_rk_sof.php
+++ sqlserver/import_rk_sof.php
@@ -41,12 +41,29 @@ if($stmt === false){ die(print_r(sqlsrv_errors(), true)); }
 while($row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC)) {
 	$row5 = date_format($row['start_time'], 'Y-m-d H:i:s');
 	$row6 = date_format($row['end_time'], 'Y-m-d H:i:s');	
-	$insertSQL = "INSERT INTO ssis_job (project_name, package_name, project_lsn, status, status_desc, start_time, end_time, elapsed_time_min) VALUES ('$row[project_name]', '$row[package_name]', '$row[project_lsn]', '$row[status]', '$row[status_desc]', '$row5', '$row6', '$row[elapsed_time_min]')";
-	print_r($insertSQL);
+	$insertSQL = "INSERT INTO ssis_job (comp_code, project_name, package_name, project_lsn, status, status_desc, start_time, end_time, elapsed_time_min) VALUES ('SOF','$row[project_name]', '$row[package_name]', '$row[project_lsn]', '$row[status]', '$row[status_desc]', '$row5', '$row6', '$row[elapsed_time_min]')";
+	// print_r($insertSQL);
     $Server01	   = "sofbi01";
     $Database02    = array("Database"=>"MisMonitor");
-    $connection02  = sqlsrv_connect($Server01, $Database02);
-	$stmt01 	   = sqlsrv_query($connection02, $insertSQL);
-	if($stmt01 === false){ die(print_r(sqlsrv_errors(), true)); }
+	$connection02  = sqlsrv_connect($Server01, $Database02);
+	
+	$sql_check = "
+	select * from ssis_job where 
+	comp_code = 'SOF' and
+	project_name = '$row[project_name]' and
+	package_name = '$row[package_name]' and
+	status_desc = '$row[status_desc]' and
+	start_time = '$row5'	
+	";
+
+	$stmt_check = sqlsrv_query($connection02, $sql_check);
+	$row_count = sqlsrv_num_rows($stmt_check);
+	print_r($row_count);
+	// if($row_count>0){
+	// 	echo "";
+	// }else{
+	// $stmt01 = sqlsrv_query($connection02, $insertSQL);
+	// if($stmt01 === false){ die(print_r(sqlsrv_errors(), true)); }
+	// }
 }
 ?>
