c9fad1101a255c8d5c6b821b322e11b63f825712
coxdoank <harryhandoko@rocketmail.com>
2021-02-15 16:12:29 +0700

    Delete sqlserver directory

--- sqlserver/import_all.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "import_daily_oto.php";
-include_once "import_rk_oto.php";
-include_once "import_overdue_oto.php";
-
-include_once "import_daily_sof.php";
-include_once "import_rk_sof.php";
-include_once "import_overdue_sof.php";
-?>
